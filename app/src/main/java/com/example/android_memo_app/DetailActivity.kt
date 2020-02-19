dd2f081763212d6cb60289b3d627b336362dee27
hyogeon <men1219@hanmail.net>
2020-02-19 19:37:44 +0900

    feat: Activity Move - Intro - List - Detail

--- app/src/main/java/com/example/android_memo_app/DetailActivity.kt
+++ app/src/main/java/com/example/android_memo_app/DetailActivity.kt
@@ -1,23 +1,19 @@
 package com.example.android_memo_app
 
-import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_detail.*
 
-import kotlinx.android.synthetic.main.activity_list.*
-
-class ListActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list)
+        setContentView(R.layout.activity_detail)
         setSupportActionBar(toolbar)
-
         fab.setOnClickListener { view ->
-            val intent = Intent(applicationContext, DetailActivity::class.java)
-            startActivity(intent)
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
         }
     }
-
 }
--- app/src/main/java/com/example/android_memo_app/IntroActivity.kt
+++ app/src/main/java/com/example/android_memo_app/IntroActivity.kt
@@ -1,10 +1,14 @@
 package com.example.android_memo_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 
 class IntroActivity : AppCompatActivity() {
+    var handler: Handler? = null
+    var runnable: Runnable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +20,25 @@ class IntroActivity : AppCompatActivity() {
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
                 View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
                 View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        runnable = Runnable {
+            val intent = Intent(applicationContext, ListActivity::class.java)
+            startActivity(intent)
+        }
+
+        handler = Handler()
+        handler?.run {
+            postDelayed(runnable, 2000)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        handler?.removeCallbacks(runnable)
     }
 }
