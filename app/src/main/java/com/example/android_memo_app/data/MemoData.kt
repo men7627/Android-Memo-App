d7db87ac1d17a8721a2794539471942fb489e473
hyogeon <men1219@hanmail.net>
2020-02-24 23:05:34 +0900

    refactor: change MemoData to RealmObject

--- app/src/main/java/com/example/android_memo_app/data/MemoData.kt
+++ app/src/main/java/com/example/android_memo_app/data/MemoData.kt
@@ -1,8 +1,11 @@
 package com.example.android_memo_app.data
 
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
 import java.util.*
 
-class MemoData (
+open class MemoData(
+    @PrimaryKey
     var id: String = UUID.randomUUID().toString(),
     var createdAt: Date = Date(),
     var title: String = "",
@@ -13,4 +16,4 @@ class MemoData (
     var longitude: Double = 0.0,
     var alarmTime: Date = Date(),
     var weather: String = ""
-)
+) : RealmObject()
