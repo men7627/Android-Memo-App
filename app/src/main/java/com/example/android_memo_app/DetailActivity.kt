7c3539a47c016da1aa2ee095fa970e14c75a4798
hyogeon <men1219@hanmail.net>
2020-02-25 01:51:20 +0900

    feat: DetailActivity Add or Update

--- app/src/main/java/com/example/android_memo_app/DetailActivity.kt
+++ app/src/main/java/com/example/android_memo_app/DetailActivity.kt
@@ -1,11 +1,21 @@
 package com.example.android_memo_app
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.android_memo_app.data.DetailViewModel
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_detail.*
 
 class DetailActivity : AppCompatActivity() {
+
+    private var viewModel: DetailViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +25,40 @@ class DetailActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        viewModel = application!!.let {
+            ViewModelProvider(viewModelStore, ViewModelProvider.AndroidViewModelFactory(it))
+                .get(DetailViewModel::class.java)
+        }
+
+        viewModel!!.let {
+            it.title.observe(this, Observer { supportActionBar?.title = it })
+            it.content.observe(this, Observer { contentEdit.setText(it) })
+        }
+
+        val memoId = intent.getStringExtra("MEMO_ID")
+        if (memoId != null) viewModel!!.loadMemo(memoId)
+
+        toolbarLayout.setOnClickListener {
+            val view = LayoutInflater.from(this).inflate(R.layout.dialog_title, null)
+            val titleEdit = view.findViewById<EditText>(R.id.titleEdit)
+
+            AlertDialog.Builder(this)
+                .setTitle("제목을 입력하세요")
+                .setView(view)
+                .setNegativeButton("취소", null)
+                .setNegativeButton("확인") { dialog, which ->
+                    supportActionBar?.title = titleEdit.text.toString()
+                }.show()
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+
+        viewModel?.addOrUpdateMemo(
+            supportActionBar?.title.toString(),
+            contentEdit.text.toString()
+        )
     }
 }
